afabd6012912e036884077050ba15f6be6239c25
Alan dev <Alan.jimenez353@gmail.com>
2024-02-22 23:07:11 -0300

    Se modifica la calidad de imagen generada

--- TwitterBotLego.py
+++ TwitterBotLego.py
@@ -47,11 +47,11 @@ try:
     prompt=prompt,
     n=1,
     size="1024x1024",
-    quality="hd" 
+    quality="standard" 
 )
 
     if response.data:
-        image_url = response.data[0].url  # Acceso directo a la propiedad 'url'
+        image_url = response.data[0].url
         print(f"URL de la imagen: {image_url}")
 
         # Determinar el nombre autoincremental de la imagen
@@ -77,29 +77,3 @@ try:
 except Exception as e:
     print(f"Ocurrió un error: {e}")
 
-
-
-
-
-
-
-
-
-
-
-""""  ------------------------------------------------------------------------- Llamado a chat GPT-3 Funciona ------------------------------------------------------------------------------------------------------------------------------------
-def obtener_respuesta():
-    prompt = "Calcula el porcentaje del año que ha transcurrido."
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-            {"role": "user", "content": prompt}
-        ]
-    )
-    return completion.choices[0].message
-
-    respuesta = obtener_respuesta()
-    print(respuesta)
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-"""     
